e7ee037cae9fe3b67eacf8b1b1ef518e61973622
René Kooi <rene@kooi.me>
2015-09-06 23:07:11 +0200

    0.0.2

--- build/emoji-styles.js
+++ build/emoji-styles.js
@@ -2,9 +2,16 @@
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-define('extplug/emoji-styles/main',['require','exports','module','extplug/Plugin'],function (require, exports, module) {
+define('extplug/emoji-styles/main',['require','exports','module','extplug/Plugin','plug/util/emoji','meld','jquery'],function (require, exports, module) {
 
   var Plugin = require('extplug/Plugin');
+  var emoji = require('plug/util/emoji');
+
+  var _require = require('meld');
+
+  var around = _require.around;
+
+  var $ = require('jquery');
 
   var SHEET_URL = 'https://extplug.github.io/emoji-styles/img/';
   var SELECTOR = 'span.emoji-inner:not(.gemoji-plug)';
@@ -49,6 +56,12 @@ define('extplug/emoji-styles/main',['require','exports','module','extplug/Plugin
       this.onChange();
     },
 
+    disable: function disable() {
+      if (this.advice) {
+        this.advice.remove();
+      }
+    },
+
     setStyle: function setStyle(className) {
       if (STYLES.indexOf(className) > -1) {
         this.settings.set('style', className);
@@ -58,10 +71,21 @@ define('extplug/emoji-styles/main',['require','exports','module','extplug/Plugin
     },
 
     onChange: function onChange() {
+      if (this.advice) {
+        this.advice.remove();
+      }
+
       var className = this.settings.get('style');
       $('body').removeClass(STYLES.map(function (name) {
         return 'extplug-emoji-style-' + name;
       }).join(' ')).addClass('extplug-emoji-style-' + className);
+
+      if (className !== 'apple') {
+        // remove variation argument for sheets without variation emoji images
+        this.advice = around(emoji, 'replacement', function (joinpoint) {
+          return joinpoint.proceedApply(joinpoint.args.slice(0, 3));
+        });
+      }
     }
   });
 
